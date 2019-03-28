c5ebd5f7431e1a79bf31b133543707d192b93475
Megadardery <Megadardery@yahoo.com>
2019-03-28 15:15:00 +0200

    Update ServerMain.java

--- Network Classes/ServerMain.java
+++ Network Classes/ServerMain.java
@@ -4,47 +4,51 @@ import java.util.Scanner;
 
 public class ServerMain {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
+    	
     	
     	System.out.println("Do you want to recieve or send? (1- recieve, 2- send)");
     	Scanner sc = new Scanner(System.in);
     	int c = sc.nextInt();
     	sc.nextLine();
     	if (c == 1){
-    		System.out.println("Enter filename: ");
+    		System.out.println("Enter Filename: ");
     		String filename = sc.nextLine();
+    		System.out.println("Enter IP Addresss: ");
+    		String ipadd = sc.nextLine();
+    		System.out.println("Enter Port: ");
+    		int portno = sc.nextInt();
     		
-    		int port = 12435;
-    		try {
-				Reciever r = new Reciever(port);
-				System.out.print("IP address is: ");
-				System.out.println(r.getIPAddress());
-				
-				System.out.print("Port is: ");
-				System.out.println(r.getPort());
-				
-				r.RecieveFile(filename);
-				System.out.println("finished");
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-    		
+    		Reciever r = new Reciever(filename,ipadd,portno);
+    		r.recieve();
     	}
     	else if (c == 2){
-    		System.out.println("Enter filename: ");
+    		int port = 31211;
+    		System.out.println("Enter Filename: ");
     		String filename = sc.nextLine();
     		
-    		System.out.println("Enter reciever IP: ");
-    		String IP = sc.nextLine();
-    		System.out.println("Enter port number");
-    		int port = sc.nextInt();
-    		try {
-				Sender.SendFile(IP, port, filename);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+    		Server r = new Server(port,filename);
+    		
+    		System.out.print("IP address is: ");
+
+			System.out.println(r.getIPAddress());
+			
+			System.out.print("Port is: ");
+			System.out.println(r.getPort());
+			r.waitForClients();
+			System.out.println("Type 'stop' to stop listening");
+			while(true){
+				sc.nextLine();
+							
+				r.stopWaiting();
+				r.sentToAllClients();
+				
+				break;
+								
 			}
+			sc.nextLine();	
     	}
+    	System.out.println("Program closed!");
+    	sc.close();
     }
 }
