111cacc065bdd28bc3805cec01f2e6ec61891618
Megadardery <Megadardery@yahoo.com>
2019-03-28 15:14:43 +0200

    Update Reciever.java

--- Network Classes/Reciever.java
+++ Network Classes/Reciever.java
@@ -1,41 +1,28 @@
 package socketproject;
 import java.io.DataInputStream;
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Reciever {
-	ServerSocket reciever;
+	String filename, IP;
 	int port;
-	Reciever(int p) throws IOException{
+	public Reciever(String f, String ip, int p){
 		port = p;
-		reciever = new ServerSocket(port);
+		filename = f;
+		IP = ip;
 	}
-	String getIPAddress() throws UnknownHostException{
-		return InetAddress.getLocalHost().getHostAddress();
-	}
-	int getPort(){
-		return port;
-	}
-	void RecieveFile(String filename) throws IOException{
-		Socket client = reciever.accept();
+	public void recieve() throws IOException{
+		Socket connect = new Socket(IP, port);
+		DataInputStream inFromServer = new DataInputStream(connect.getInputStream());
+		int len = inFromServer.readInt();
+		byte[] arg = new byte[len];
 		
-        DataInputStream inFromClient = new DataInputStream(client.getInputStream());
-
-        int size = inFromClient.readInt();
-        byte[] arg = new byte[size];
-        
-        inFromClient.readFully(arg);
-        
-        Files.write(Paths.get(filename),arg);
-        
-        client.close();
-	}
-	protected void finalize() throws IOException{
-		reciever.close();
+		inFromServer.readFully(arg);
+		
+		Files.write(Paths.get(filename), arg);
+		
+		connect.close();
 	}
 }
